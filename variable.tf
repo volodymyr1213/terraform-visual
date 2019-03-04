acd61a93c6450d27c0d6be634e923649f456c704
Volodymyr Khomenko <45836705+volodymyr1213@users.noreply.github.com>
2019-03-04 16:56:48 -0600

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "ami" {}
+variable "instance_type" {}
+variable "keyname" {}
+variable "Name" {}
+variable "subnet_id" {}
+variable "Env" {}
+variable "Dept" {}
+variable "Created_by" {}
+variable "count" {}
+variable "min_size" {}
+variable "max_size" {}
