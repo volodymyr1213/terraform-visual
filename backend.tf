28d6b51bbdc6aafe27d31c954dff446c11ce9762
Volodymyr Khomenko <45836705+volodymyr1213@users.noreply.github.com>
2019-05-04 16:38:51 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "terraform-visual-murodbey"
+        bucket = "terraform-visual-volodymyr13"
         key = "somekey"
         region = "eu-west-1"
     }
